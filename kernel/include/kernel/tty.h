f66ea7cb6fe18b342335fffda248e2b6048a8ef3
maxijp <max.r.prime@gmail.com>
2026-06-20 23:00:07 +0100

    Adding the GPL license to my code.

--- kernel/include/kernel/tty.h
+++ kernel/include/kernel/tty.h
@@ -1,3 +1,22 @@
+/*
+	This file is part of Max Unix.
+
+	Max Unix is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	Max Unix is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Max Unix.  If not, see <https://www.gnu.org/licenses/>.
+
+	Copyright (c) Max Prime 2026. All rights reserved.
+*/
+
 #ifndef _KERNEL_TTY_H
 #define _KERNEL_TTY_H
 
